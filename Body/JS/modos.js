46b579ccb9dd6667a72afdf8bd628476a3e9c431
Brayan MG <107977576+Brayan-CG@users.noreply.github.com>
2022-12-27 21:47:47 -0500

    Add files via upload

--- Body/JS/modos.js
+++ Body/JS/modos.js
@@ -1,6 +1,24 @@
 function atras(){
    location.href="inicio2.html"
 }
+function primero(){
+   location.href="go:1"
+}
+function segundo(){
+   location.href="go:2"
+}
+function tercero(){
+   location.href="go:3"
+}
+function cuarto(){
+   location.href="go:4"
+}
+function quinto(){
+   location.href="go:5"
+}
+function sesto(){
+   location.href="go:6"
+}
 function hugo(){
    location.href="Hugo PTP/hugo.html"
 }
